a35bcac619d036846659099eb7ee12d7895d76ba
ikitamonday <ikitamonday@hotmail.com>
2015-03-04 15:04:56 +0100

    ein Eintrag

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log("Ein JS Beispiel");
+console.log("Ist aber noch nicht fertig");
+
+alert("DO NOT USE In PRODUCTION");
+alert("BAD THINGS WILL HAPPEN");
+
+document.body.style.backgrondColor = 'red';
+
+var yes = confitm("Are you sure");
+if (!yes) {
+	windows.close();
+};
