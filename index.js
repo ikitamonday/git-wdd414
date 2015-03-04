c2ca29eda214542a1dee4a8a668683178d1bc9f0
ikitamonday <ikitamonday@hotmail.com>
2015-03-04 15:20:52 +0100

    Revert 0c84083..766397c
    This rolls back to commit 0c84083bd00da4f03ce61363ed1cbf12a2b6365d.

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 console.log("Ein JS Beispiel");
+console.log("Ist aber noch nicht fertig");
 
+alert("DO NOT USE In PRODUCTION");
+alert("BAD THINGS WILL HAPPEN");
+
+document.body.style.backgrondColor = 'red';
+
+var yes = confitm("Are you sure");
+if (!yes) {
+	windows.close();
+};
+
+alert("Das ist noch ein Eintrag");
